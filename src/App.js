bbfcbf73b19cfdcfbc83a5026af6b3a7efbde202
idris bohra <idrisbohra786@gmail.com>
2024-03-19 22:00:30 +0530

    Routes has been set up

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import { RouterProvider } from "react-router-dom";
-import router from "./router";
+import React from "react";
+import { Route, Routes } from 'react-router-dom'
+import Home from "./pages/home/home";
+import Menu from "./pages/menu/menu";
+import Offers from "./pages/offers/offers";
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <React.Fragment>
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route exact path='/menu' element={<Menu />} />
+        <Route exact path='/offers' element={<Offers />} />
+      </Routes>
+    </React.Fragment>
+  )
 }
 
 export default App;
