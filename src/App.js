7bb98a25de74d5a4837292979baf97eea3112d81
kannan-ravi <kannanravindran00kr@gmail.com>
2024-03-20 19:00:14 +0530

    changed css for gap from navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React from "react";
-import { Navigate, Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/home";
 import Menu from "./pages/menu/menu";
 import Offers from "./pages/offers/offers";
 import Navbar from "./components/navbar/navbar";
 import NotFound from "./pages/notFound/notFound";
-import './scss/import.scss';
+import "./scss/import.scss";
+import Footer from "./components/footer/footer";
 
 function App() {
   return (
     <React.Fragment>
       <Navbar />
       <Routes>
-        <Route exact path='/' element={<Navigate to={'/home'} />} />
-        <Route exact path='/home' element={<Home />} />
-        <Route exact path='/menu' element={<Menu />} />
-        <Route exact path='/offers' element={<Offers />} />
-        <Route exact path='*' element={<NotFound />} />
+        <Route exact path="/" element={<Navigate to={"/home"} />} />
+        <Route exact path="/home" element={<Home />} />
+        <Route exact path="/menu" element={<Menu />} />
+        <Route exact path="/offers" element={<Offers />} />
+        <Route exact path="*" element={<NotFound />} />
       </Routes>
+      <Footer />
     </React.Fragment>
-  )
+  );
 }
 
 export default App;
--- src/pages/menu/menu.jsx
+++ src/pages/menu/menu.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Menu = () => {
   return (
-    <div>Menu</div>
+    <div className='gap-from-navbar'>Menu</div>
   )
 }
 
